6f9f8100d2194c4ef503685f436310189879a423
matthew-schultz <mschultz@hawaii.edu>
2016-10-09 22:23:38 -1000

    Task 3: Tasklist form
    use var instead of let, otherwise it hangs on compile

--- app/tasklist.js
+++ app/tasklist.js
@@ -1,6 +1,9 @@
 /**
  * Created by matthew on 10/9/2016.
  */
+/**
+ * Created by matthew on 10/9/2016.
+ */
 Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isClient) {
@@ -8,6 +11,27 @@ if (Meteor.isClient) {
   Template.tasks.helpers({
     tasks: function(){
       return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+
+  Template.tasks.events({
+    "submit .add-task": function(event){
+      var name = event.target.name.value;
+
+      Tasks.insert({
+        name: name,
+        createdAt: new Date()
+      });
+
+      event.target.name.value = '';
+
+      return false;
+    },
+    "click .delete-task": function(event){
+      if(confirm('Delete Task?')){}
+      Tasks.remove(this._id)
+      return false;
     }
   });
 }
