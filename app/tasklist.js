9bec035c507e100c2b3853c74d5e301ecff5837b
matthew-schultz <mschultz@hawaii.edu>
2016-10-09 22:47:18 -1000

    Task 4: Tasklist authentication
    added user authentication/more security

--- app/tasklist.js
+++ app/tasklist.js
@@ -19,19 +19,35 @@ if (Meteor.isClient) {
     "submit .add-task": function(event){
       var name = event.target.name.value;
 
-      Tasks.insert({
-        name: name,
-        createdAt: new Date()
-      });
+      Meteor.call('addTask', name);
 
       event.target.name.value = '';
 
       return false;
     },
     "click .delete-task": function(event){
-      if(confirm('Delete Task?')){}
-      Tasks.remove(this._id)
+      if(confirm('Delete Task?')){
+        Meteor.call('deleteTask', this._id);
+      }
+
       return false;
     }
   });
 }
+
+Meteor.methods({
+  addTask: function(name){
+    if(!Meteor.userId()){
+      throw new Meteor.Error('No Access!');
+    }
+    Tasks.insert({
+      name: name,
+      createdAt: new Date(),
+      userId: Meteor.userId()
+    });
+  },
+
+  deleteTask: function(taskId){
+    Tasks.remove(taskId);
+  }
+});
